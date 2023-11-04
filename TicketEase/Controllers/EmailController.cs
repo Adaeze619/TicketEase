a7cefe109b7376494970b27154aec50621881a85
CHUKWUDI INNOCENT <125125776+Innochu@users.noreply.github.com>
2023-11-04 15:28:01 +0100

    implemented email and user controllers that geberates mail

---
+++ TicketEase/Controllers/EmailController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicketEase.Controllers
+{
+    [ProducesResponseType(200)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(500)]
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailService _emailService;
+        private readonly ILogger<EmailController> _logger;
+
+        public EmailController(IEmailService emailService, ILogger<EmailController> logger)
+        {
+            _emailService = emailService;
+            _logger = logger;
+        }
+
+        [HttpPost("SendMail")]
+       
+        public async Task<IActionResult> SendMail()
+        {
+            try
+            {//"jemmimahabdul@gmail.com";
+                //"chuksinnocent1@gmail.com"
+                MailRequest mailRequest = new MailRequest();
+                mailRequest.ToEmail = "kely2000sgm@gmail.com";
+                mailRequest.Subject = "Welcome to Tech world";
+                mailRequest.Body = GetHtmlContent();
+                await _emailService.SendEmailAsync(mailRequest);
+                return Ok(mailRequest);
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, "Email sending failed."); //logger not fully implemented
+
+                // Return an error response to the client
+                return StatusCode(500, "Email sending failed. Please try again later.");
+            }
+
+
+        }
+        private string GetHtmlContent()
+        {
+            string response = "<div style=\"width:100%; background-color: lightblue; text-align:center;\">";
+            response += "<h1>Welcome To SQD 17 Ticketing app</h1>";
+            response += "<img src=\"https://www.google.com/imgres?imgurl=https%3A%2F%2Fusa.bootcampcdn.com%2Fwp-content%2Fuploads%2Fsites%2F108%2F2021%2F12%2Ftes_gen_blog_post_071921_1233182206-1-800x412.jpg&tbnid=zOY9WNHB7uLPnM&vet=12ahUKEwis_Irgw6iCAxVSpycCHSXNDWsQMygLegUIARCEAQ..i&imgrefurl=https%3A%2F%2Fbootcamp.cvn.columbia.edu%2Fblog%2Fsoftware-engineer-vs-developer%2F&docid=iVux8Sn2xBjURM&w=800&h=412&q=developers&ved=2ahUKEwis_Irgw6iCAxVSpycCHSXNDWsQMygLegUIARCEAQ\" alt=\"Your Image\">";
+            response += "<h2>Thanks for Registering with us</h2>";
+            response += "<a href=\"https://www.youtube.com/watch?v=VkrKNXscoto&list=PL6n9fhu94yhWi8K02Eqxp3Xyh_OmQ0Rp6&index=3\">Please Join us by clicking this link</a>";
+
+            response += "<div><h1>Contact us: everythingtech@gmail.com</h1></div>";
+            response += "</div>";
+
+            return response;
+        }
+
+    }
+}
---
+++ TicketEase/Controllers/UserController.cs
@@ -0,0 +1,231 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace TicketEase.Controllers
+{
+    [ProducesResponseType(200)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(500)]
+    [ProducesResponseType(401)]
+    [ProducesResponseType(201)]
+    [ProducesResponseType(404)]
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+        private readonly ILogger<UserController> _logger;
+        private readonly IEmailService _emailService;
+
+        public UserController(DataContext context, ILogger<UserController> logger, IEmailService emailService)
+        {
+            _context = context;
+            _logger = logger;
+            _emailService = emailService;
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegisterRequest request)
+        {
+            if (_context.Users.Any(u => u.Email == request.Email))
+            {
+                return BadRequest("User already exists.");
+            }
+
+            CreatePasswordHash(request.Password,
+                 out byte[] passwordHash,
+                 out byte[] passwordSalt);
+
+            var user = new User
+            {
+                Email = request.Email,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                VerificationToken = CreateRandomToken()
+            };
+
+
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            // Generate a verification link
+            // var verificationLink = $"https://yourwebsite.com/verify?token={user.VerificationToken}";
+
+            // Send a registration email
+            var mailRequest = new MailRequest
+            {
+                ToEmail = user.Email, // User's email address
+                Subject = "Welcome to Your Website - Email Verification",
+                Body = "Thank you for registering on our website. Please verify your email by clicking this link: $\"https://yourwebsite.com/verify?token={user.VerificationToken}\"",
+            };
+
+            await _emailService.SendEmailAsync(mailRequest);
+
+
+            // Log successful registration and email sending
+            _logger.LogInformation($"User registration successful for email: {user.Email}");
+            return Ok("User successfully created! Please check your email for verification instructions.");
+
+
+        }
+
+
+
+
+        [HttpPost("login")]
+
+        public async Task<IActionResult> Login(UserLoginRequest request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+            if (user == null)
+            {
+                return BadRequest("User not found.");
+            }
+
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Password is incorrect.");
+            }
+
+            if (user.VerifiedAt == null)
+            {
+                return BadRequest("Not verified!");
+            }
+
+            var mailRequest = new MailRequest
+            {
+                ToEmail = user.Email, // User's email address
+                Subject = "Login Successful!!",
+                Body = "You have successfully Login...If this was not you, click on this link to reset your password! https://yourwebsite.com/verify?token  ",
+            };
+
+            await _emailService.SendEmailAsync(mailRequest);
+
+            _logger.LogInformation($"User Login successful: {user.Email}");
+            return Ok($"Login Successful, {user.Email}! ");
+        }
+
+        [HttpPost("verify")]
+
+        public async Task<IActionResult> Verify(string token)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.VerificationToken == token);
+            if (user == null)
+            {
+                return BadRequest("Invalid token.");
+            }
+
+            user.VerifiedAt = DateTime.Now;
+            await _context.SaveChangesAsync();
+
+
+            var mailRequest = new MailRequest
+            {
+                ToEmail = user.Email, // User's email address
+                Subject = "Account Verification!",
+                Body = "You have successfully verified your email...If this was not you, click on this link to reset your password! https://yourwebsite.com/verify?token  ",
+            };
+
+            await _emailService.SendEmailAsync(mailRequest);
+
+            _logger.LogInformation($"User successfully Verified: {user.Email}");
+            return Ok($"Verification Successful, {user.Email}! ");
+
+
+        }
+
+        [HttpPost("forgot-password")]
+
+        public async Task<IActionResult> ForgotPassword(string email)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+            if (user == null)
+            {
+                return BadRequest("User not found.");
+            }
+
+            user.PasswordResetToken = CreateRandomToken();
+            user.ResetTokenExpires = DateTime.Now.AddDays(3);
+            await _context.SaveChangesAsync();
+
+            var mailRequest = new MailRequest
+            {
+                ToEmail = user.Email, // User's email address
+                Subject = "Forgot Password?",
+                Body = " Click on the link below to reset your password  https://yourwebsite.com/verify?token  ",
+            };
+
+            await _emailService.SendEmailAsync(mailRequest);
+
+            _logger.LogInformation($"Forgot password successfully initiated: {user.Email}");
+            return Ok($"You may now reset your password, {user.Email}! ");
+
+
+        }
+
+        [HttpPost("reset-password")]
+
+        public async Task<IActionResult> ResettPassword(ResetPasswordRequest request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.PasswordResetToken == request.Token);
+            if (user == null || user.ResetTokenExpires < DateTime.Now)
+            {
+                return BadRequest("Invalid Token.");
+            }
+
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            user.PasswordResetToken = request.Token;
+            user.ResetTokenExpires = null;
+
+            await _context.SaveChangesAsync();
+
+
+
+            var mailRequest = new MailRequest
+            {
+                ToEmail = user.Email, // User's email address
+                Subject = "Password Reset!",
+                Body = " You have successfully reset your password.. Kindly Click here https://yourwebsite.com/verify?token  to Login",
+            };
+
+            await _emailService.SendEmailAsync(mailRequest);
+
+            _logger.LogInformation($"Forgot password successfully initiated: {user.Email}");
+            return Ok($"Password successfully reset, {user.Email}! ");
+
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac
+                    .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac
+                    .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+
+        private string CreateRandomToken()
+        {
+            return Convert.ToHexString(RandomNumberGenerator.GetBytes(64));
+        }
+    }
+}
