0e55cc87539d7fd7edbb6e4bc2e3595c74a731e8
Thygodfada <nestoloway@gmail.com>
2023-11-03 22:16:08 -0400

    SeedingUserRoles

--- TicketEase.Persistence/Context/TicketEaseDbContext.cs
+++ TicketEase.Persistence/Context/TicketEaseDbContext.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TicketEase.Common.Utilities;
 
 namespace TicketEase.Persistence.Context
 {
-    internal class TicketEaseDbContext
+    public class TicketEaseDbContext: IdentityDbContext
     {
+
+        public TicketEaseDbContext(DbContextOptions<TicketEaseDbContext> options) 
+            : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Seeder.SeedData(modelBuilder);
+        }
     }
+
+    
 }
+ 
