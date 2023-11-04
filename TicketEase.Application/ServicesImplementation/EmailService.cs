66bb924b3162dd825724c08affc760cfb63be04e
CHUKWUDI INNOCENT <125125776+Innochu@users.noreply.github.com>
2023-11-04 15:03:06 +0100

    created service and interface for our email implementation

---
+++ TicketEase.Application/ServicesImplementation/EmailService.cs
@@ -0,0 +1,81 @@
+﻿using Email2.Emailfolder;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using MimeKit.Text;
+
+
+namespace TicketEase.Application.ServicesImplementation
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailSettings emailSettings;
+        public EmailService(IOptions<EmailSettings> options)
+        {
+            this.emailSettings = options.Value;
+        }
+        public async Task SendEmailAsync(MailRequest mailRequest)
+        {
+            var email = new MimeMessage();
+
+            email.Sender = MailboxAddress.Parse(emailSettings.Email);
+            email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
+            email.Subject = mailRequest.Subject;
+            var builder = new BodyBuilder();
+            builder.HtmlBody = mailRequest.Body;
+            email.Body = builder.ToMessageBody();
+
+            byte[] fileBytes1;
+            byte[] fileBytes2;
+
+            if (System.IO.File.Exists("Attachment/img8.jpg") && System.IO.File.Exists("Attachment/img3.jpg"))
+            {
+                // Attach the first file
+                using (var ms1 = new MemoryStream())
+                {
+                    using (FileStream file1 = new FileStream("Attachment/img8.jpg", FileMode.Open, FileAccess.Read))
+                    {
+                        file1.CopyTo(ms1);
+                        fileBytes1 = ms1.ToArray();
+                    }
+                    builder.Attachments.Add("attachment1.jpg", fileBytes1, ContentType.Parse("application/octet-stream"));
+                }
+
+                // Attach the second file
+                using (var ms2 = new MemoryStream())
+                {
+                    using (FileStream file2 = new FileStream("Attachment/img3.jpg", FileMode.Open, FileAccess.Read))
+                    {
+                        file2.CopyTo(ms2);
+                        fileBytes2 = ms2.ToArray();
+                    }
+                    builder.Attachments.Add("attachment2.jpg", fileBytes2, ContentType.Parse("application/octet-stream"));
+                }
+            }
+
+            builder.HtmlBody = mailRequest.Body;
+            email.Body = builder.ToMessageBody();
+
+            using var smtp = new SmtpClient();
+            try
+            {
+                smtp.Connect(emailSettings.Host, emailSettings.Port, SecureSocketOptions.StartTls);
+                smtp.Authenticate(emailSettings.Email, emailSettings.Password);
+                await smtp.SendAsync(email);
+                // smtp.Disconnect(true);
+            }
+            catch (Exception ex)
+            {
+
+                throw new InvalidOperationException(ex.Message);
+            }
+            finally
+            {
+                smtp.Disconnect(true);
+
+                smtp.Dispose();
+            }
+        }
+    }
+}
--- TicketEase/Program.cs
+++ TicketEase/Program.cs
@@ -7,6 +7,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
+builder.Services.AddTransient<IEmailService, EmailService>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
